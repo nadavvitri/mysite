1c4300934bdac8b581fa2b09b10cbf23ece43635
Nadav Vitri <nadavvitri@Nadavs-MacBook-Pro.local>
2025-07-28 10:14:22 +0300

    remove staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,10 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_URL = '/static/'
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
