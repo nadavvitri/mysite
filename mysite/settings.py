cd64a3d196de190dcfeda55354e3c46037363666
Nadav Vitri <nadavvitri@Nadavs-MacBook-Pro.local>
2025-07-28 10:28:54 +0300

    fixed static files dir

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 DEBUG = False
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
